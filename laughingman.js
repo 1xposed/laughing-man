573538e8afee5e7dfced486f7f91b1385f70d111
1xposed <karstenesserr@gmail.com>
2025-11-19 10:08:08 -0500

    added JS

---
+++ laughingman.js
@@ -0,0 +1,12 @@
+
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    let squareElement = document.getElementById("square");
+
+    squareElement.addEventListener("click", function(){
+        alert("OMG YOU CAUGHT ME!");
+
+    });
+
+});
