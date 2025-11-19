1d16350ce21b4b440f4e3a4bb2a23a082c708bd4
1xposed <karstenesserr@gmail.com>
2025-11-19 10:34:38 -0500

    Started loop for children objectors

--- laughingman.js
+++ laughingman.js
@@ -1,3 +1,8 @@
+const SQUARE_COUNT = 3;
+const SPEED = 5;
+const TIMER_SPEED = 100;
+
+
 
 
 document.addEventListener('DOMContentLoaded', function(){
@@ -9,4 +14,32 @@ document.addEventListener('DOMContentLoaded', function(){
 
     });
 
+    let box = document.querySelector("#box");
+
+    for(let i = 0;i<SQUARE_COUNT;i++){
+        let square = document.createElement('img');
+        square.src = 'laughing_man.jpg';
+        square.alt = 'Catch the Laughing Man!';
+        square.className = 'square';
+        box.appendChild(square);
+
+        
+
+
+    }
+
+    let allSquares = box.children;
+    let squareArray = Array.from(children)
+    squareArray.forEach(function(element, index){
+
+        var dx = SPEED * Math.random()*2-1;
+        var dy = SPEED * Math.random()*2-1;
+
+
+    });
+
+    
+
 });
+
+
