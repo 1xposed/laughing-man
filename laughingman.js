1661045e7e1785a18857db60f61c1ccba038d97e
1xposed <karstenesserr@gmail.com>
2025-12-02 22:16:32 -0500

    added mouseover and mouse out as well as an error within line 66 of the javaScript file +dx -> +dy.

--- laughingman.js
+++ laughingman.js
@@ -1,5 +1,5 @@
-const SQUARE_COUNT = 3;
-const SPEED = 5;
+const SQUARE_COUNT = 15;
+const SPEED = 10;
 const TIMER_SPEED = 100;
 
 
@@ -21,6 +21,19 @@ document.addEventListener('DOMContentLoaded', function(){
         square.src = 'laughing_man.jpg';
         square.alt = 'Catch the Laughing Man!';
         square.className = 'square';
+
+        square.addEventListener('mouseover',()=>{
+            square.src='orochimaru_50x50.jpg';
+            //changes to new pic
+        });
+
+        square.addEventListener('mouseout',() =>{
+            square.src='laughing_man.jpg';
+            //brings back OG laughing man pic
+
+        });
+
+
         box.appendChild(square);
 
         
@@ -29,11 +42,30 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
     let allSquares = box.children;
-    let squareArray = Array.from(children)
+    let squareArray = Array.from(allSquares)
     squareArray.forEach(function(element, index){
 
-        var dx = SPEED * Math.random()*2-1;
-        var dy = SPEED * Math.random()*2-1;
+        var dx = SPEED * (Math.random()*2-1);
+        var dy = SPEED * (Math.random()*2-1);
+
+        setInterval(() => {
+            let currentLeft = parseFloat(element.style.left) || 225;
+            let currentTop =  parseFloat(element.style.top) || 175;
+
+            if(currentLeft >= 450 || currentLeft <= 0){
+                dx*=-1;
+                element.style.borderColor = getRandomColor();
+
+            }
+            if(currentTop >=350 || currentTop <=0){
+                dy*= -1;
+                element.style.borderColor = getRandomColor();
+            }
+            
+            element.style.left = (currentLeft+dx)+"px"
+            element.style.top = (currentTop+dy)+"px"
+     } , TIMER_SPEED);
+
 
 
     });
@@ -41,5 +73,13 @@ document.addEventListener('DOMContentLoaded', function(){
     
 
 });
+
+function getRandomColor() {
+    let r = parseInt(Math.random() * 256);
+    let g = parseInt(Math.random() * 256);
+    let b = parseInt(Math.random() * 256);
+    let color = `rgb(${r},${g},${b})`;
+    return color;
+}
 
 
